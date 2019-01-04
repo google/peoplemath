205e249a7c70cbde38b9197592c2b62ddb4a7caf
Andrew Medworth <amedworth@google.com>
2019-01-04 18:19:49 +0000

    Bucket does not require an id

--- src/app/bucket.ts
+++ src/app/bucket.ts
@@ -1,7 +1,6 @@
 import { Objective } from "./objective";
 
 export class Bucket {
-    id: string;
     displayName: string;
     allocationPercentage: number;
     objectives?: Objective[] = [];
--- src/app/okrstorage.service.ts
+++ src/app/okrstorage.service.ts
@@ -9,17 +9,17 @@ import { of } from 'rxjs/observable/of';
 // TODO Move to backend storage service
 
 const BUCKETS: Bucket[] = [
-  { id: 'a', displayName: 'First bucket', allocationPercentage: 40, objectives: [
+  { displayName: 'First bucket', allocationPercentage: 40, objectives: [
     { name: 'A bucket 1 thing', resourceEstimate: 6 },
     { name: 'Another bucket 1 thing', resourceEstimate: 3, assignments: [
       { personId: 'alice', commitment: 2 },
     ] },
   ] },
-  { id: 'b', displayName: 'Second bucket', allocationPercentage: 40, objectives: [
+  { displayName: 'Second bucket', allocationPercentage: 40, objectives: [
     { name: 'A bucket 2 thing', resourceEstimate: 12 },
     { name: 'Another bucket 2 thing', resourceEstimate: 1 },
   ] },
-  { id: 'c', displayName: 'Third bucket', allocationPercentage: 20, objectives: [
+  { displayName: 'Third bucket', allocationPercentage: 20, objectives: [
     { name: 'A bucket 3 thing', resourceEstimate: 3 },
   ] },
 ];
