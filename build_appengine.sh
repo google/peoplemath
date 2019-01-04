381cf90a0506858554f66473078473f20d4cd0b3
Andrew Medworth <amedworth@google.com>
2019-01-04 18:56:58 +0000

    Fix build_appengine.sh

--- build_appengine.sh
+++ build_appengine.sh
@@ -2,7 +2,7 @@
 # Prepare prod app for deployment to Google AppEngine using gcloud app deploy
 
 if [[ -e appengine_dist ]]; then
-    echo Remove appengine_dist (e.g. via rm -rf) first!
+    echo Remove appengine_dist first, e.g. via rm -rf
     exit 1
 fi
 
@@ -11,4 +11,4 @@ mkdir appengine_dist
 ng build --prod
 cp backend/* appengine_dist
 mkdir appengine_dist/static
-cp dist/* appengine_dist/static
+cp dist/peoplemath/* appengine_dist/static
