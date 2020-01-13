f14f9eec7381a24469bbefa77fa7a54ebe63fb83
Andrew Medworth <amedworth@google.com>
2020-01-13 21:12:07 +0000

    Remove orphaned hashbang

--- build_appengine.sh
+++ build_appengine.sh
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
 # Prepare prod app for deployment to Google App Engine using gcloud app deploy
 
 if [[ -e appengine_dist ]]; then
