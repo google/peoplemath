f8641eabe38a0dc009d0d7b7c980308b29afc79b
Andrew Medworth <amedworth@google.com>
2020-02-19 10:31:00 +0000

    Update to Angular 9

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
