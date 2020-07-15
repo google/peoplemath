26c2ca9bcfeb869f86f1e124849a6a54cd6201fb
Vladimir <vladimir.teplov@gmail.com>
2020-07-15 21:26:30 +0300

    Refactor backend into multiple Go packages (#40)

---
+++ backend/storage/storage.go
@@ -0,0 +1,34 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"context"
+	"peoplemath/models"
+)
+
+// StorageService to represent the persistent store
+type StorageService interface {
+	GetAllTeams(ctx context.Context) ([]models.Team, error)
+	GetTeam(ctx context.Context, teamID string) (models.Team, bool, error)
+	CreateTeam(ctx context.Context, team models.Team) error
+	UpdateTeam(ctx context.Context, team models.Team) error
+	GetAllPeriods(ctx context.Context, teamID string) ([]models.Period, bool, error)
+	GetPeriod(ctx context.Context, teamID, periodID string) (models.Period, bool, error)
+	CreatePeriod(ctx context.Context, teamID string, period models.Period) error
+	UpdatePeriod(ctx context.Context, teamID string, period models.Period) error
+	GetSettings(ctx context.Context) (models.Settings, error)
+	Close() error
+}
