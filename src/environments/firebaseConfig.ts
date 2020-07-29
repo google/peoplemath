6bc6908999e139a0141205d58b452093e2fb2edd
Samrthi <samicolon@google.com>
2020-07-29 11:10:43 +0100

    update config

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2020 Google LLC
+ */
+
+export const firebaseConfig = {
+  firebase: {
+    apiKey: 'your-api-key',
+    authDomain: 'your-auth-domain',
+    projectId: 'your-project-id',
+    appId: 'your-app-id',
+  }
+};
