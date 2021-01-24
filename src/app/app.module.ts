ee83ebba629066a099ef34ebda2ccc3d43b03c5b
Andrew Medworth <github@medworth.org.uk>
2021-01-24 15:37:08 +0000

    EditPersonDialog -> EditPersonDialogComponent (#113)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { PeriodComponent } from './period/period.component';
 import { StorageService } from './storage.service';
 import { BucketComponent } from './bucket/bucket.component';
 import { ObjectiveComponent } from './objective/objective.component';
-import { PeopleComponent, EditPersonDialog } from './people/people.component';
+import { PeopleComponent, EditPersonDialogComponent } from './people/people.component';
 import { AssignmentsByPersonComponent } from './assignments-by-person/assignments-by-person.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
@@ -68,7 +68,7 @@ import { DisplayObjectivesPipe } from './bucket/displayobjectives.pipe';
     BucketComponent,
     ObjectiveComponent,
     PeopleComponent,
-    EditPersonDialog,
+    EditPersonDialogComponent,
     AssignmentsByPersonComponent,
     AssignmentDialogComponent,
     EditObjectiveDialogComponent,
