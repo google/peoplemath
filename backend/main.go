33e8cdaae4ad8cb8dadca3988370fc802a421697
Andrew Medworth <amedworth@google.com>
2021-02-09 21:08:47 +0000

    Explicitly log ListenAndServe exit

--- backend/main.go
+++ backend/main.go
@@ -102,5 +102,6 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), handler))
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler)
+	log.Fatalf("ListenAndServe exited: %s", err)
 }
