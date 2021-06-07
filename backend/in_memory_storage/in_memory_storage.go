google/peoplemath

commit 47d5d4b61bf8c1e63fb9ba9ce9f0df4cc515cc9a

Bucket: Update reordering style and animation

src/app/bucket/bucket.component.css
@@ -1,22 +1,53 @@
-.header-button {
+app-bucket .header-button {
   float: right;
 }
-.order-buttons {
+
+app-bucket .order-buttons {
   margin-right: 10px;
 }
-.mat-icon-button {
+
+app-bucket .mat-icon-button {
   width: 30px;
 }
-.reorder-icon {
-  vertical-align: middle;
-  padding-right: 10px;
-  padding-bottom: 15px;
+
+app-bucket .cdk-drop-list-dragging mat-list-item {
+  background-color: #fff;
+  cursor: move;
 }
-.app-mat-list .mat-list-item {
-  height: auto;
+
+app-bucket .mat-list-item-content {
+  display: flex;
+}
+
+app-bucket .cdk-drag-placeholder {
+  background-color: rgba(0, 0, 0, 0.04);
+  cursor: move;
   min-height: 40px;
+  transition: var(--transition); /* From <body> */
 }
-.mat-list-item app-objective {
-  padding-bottom: 15px;
+
+app-bucket .cdk-drag-handle,
+.cdk-drag-preview-app-bucket .cdk-drag-handle {
+  color: #616161;
+  cursor: move;
+  padding-right: 8px;
+}
+
+app-bucket app-objective,
+.cdk-drag-preview-app-bucket app-objective {
   width: 100%;
+}
+
+/* These elements exist in the global scope and not within <app-bucket> */
+.cdk-drag-preview-app-bucket {
+  background-color: #fff;
+  display: flex;
+  font-size: 12px;
+}
+
+.cdk-drag-preview-app-bucket .mat-list-item-content {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  padding: 0 16px;
 }

src/app/bucket/bucket.component.html
@@ -20,36 +20,38 @@
 </mat-card-subtitle>
 </mat-card-header>
 <mat-card-content>
-<mat-list dense *ngIf="!showOrderButtons" class="app-mat-list">
-    <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives ">
-    <app-objective
-        [objective]="displayObjective.objective" [unit]="unit"
-        [unallocatedTime]="unallocatedTime"
-        [isEditingEnabled]="isEditingEnabled"
-        [isReorderingEnabled]="showOrderButtons"
-        [otherBuckets]="otherBuckets"
-        [resourcesCumulativeSum]="displayObjective.cumulativeSum"
-        [bucketAllocationLimit]="bucketAllocationLimit()"
-        (moveBucket)="moveObjective($event[0], $event[1], $event[2])"
-        (delete)="deleteObjective($event)"
-        (changed)="onObjectiveChanged($event[0], $event[1])"></app-objective>
-    </mat-list-item>
+<mat-list dense *ngIf="!showOrderButtons">
+  <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives">
+    <ng-container *ngTemplateOutlet="objective; context: {$implicit: displayObjective}">
+    </ng-container>
+  </mat-list-item>
 </mat-list>
-<mat-list dense *ngIf="showOrderButtons" cdkDropList (cdkDropListDropped)="reorderDrop($event)" class="app-mat-list">
-    <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives" cdkDrag>
-        <i class="material-icons reorder-icon" cdkDragHandle>reorder</i>
-        <app-objective
-            [objective]="displayObjective.objective" [unit]="unit"
-            [unallocatedTime]="unallocatedTime"
-            [isEditingEnabled]="isEditingEnabled"
-            [isReorderingEnabled]="showOrderButtons"
-            [otherBuckets]="otherBuckets"
-            [resourcesCumulativeSum]="displayObjective.cumulativeSum"
-            [bucketAllocationLimit]="bucketAllocationLimit()"
-            (moveBucket)="moveObjective($event[0], $event[1], $event[2])"
-            (delete)="deleteObjective($event)"
-            (changed)="onObjectiveChanged($event[0], $event[1])"></app-objective>
-    </mat-list-item>
+<mat-list dense *ngIf="showOrderButtons" cdkDropList (cdkDropListDropped)="reorderDrop($event)">
+  <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives"
+      cdkDrag
+      [cdkDragPreviewClass]="['cdk-drag-preview-app-bucket', 'mat-elevation-z1']">
+    <div *cdkDragPlaceholder></div>
+    <i class="material-icons" cdkDragHandle>reorder</i>
+    <ng-container *ngTemplateOutlet="objective; context: {$implicit: displayObjective}">
+    </ng-container>
+  </mat-list-item>
 </mat-list>
 </mat-card-content>
 </mat-card>
+
+<!-- Reusable template for <app-objective> -->
+<ng-template #objective let-displayObjective>
+  <app-objective
+      [objective]="displayObjective.objective"
+      [unit]="unit"
+      [unallocatedTime]="unallocatedTime"
+      [isEditingEnabled]="isEditingEnabled"
+      [isReorderingEnabled]="showOrderButtons"
+      [otherBuckets]="otherBuckets"
+      [resourcesCumulativeSum]="displayObjective.cumulativeSum"
+      [bucketAllocationLimit]="bucketAllocationLimit()"
+      (moveBucket)="moveObjective($event[0], $event[1], $event[2])"
+      (delete)="deleteObjective($event)"
+      (changed)="onObjectiveChanged($event[0], $event[1])">
+  </app-objective>
+</ng-template>

src/app/bucket/bucket.component.ts
@@ -13,12 +13,13 @@
 // limitations under the License.
 
 import {
-  Component,
-  OnInit,
-  Input,
-  Output,
-  EventEmitter,
   ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation,
 } from '@angular/core';
 import { Bucket, ImmutableBucket } from '../bucket';
 import { CommitmentType, ImmutableObjective } from '../objective';
@@ -40,6 +41,7 @@ import { ObjectiveComponent } from '../objective/objective.component';
   styleUrls: ['./bucket.component.css'],
   // Requires all inputs to be immutable
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class BucketComponent implements OnInit {
   @Input() bucket?: ImmutableBucket;

src/styles.css
@@ -4,8 +4,12 @@ body {
 }
 
 body {
-  margin: 0;
+  --transition: transform 200ms cubic-bezier(0, 0, 0.2, 1);
   font-family: Roboto, 'Helvetica Neue', sans-serif;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+  text-rendering: optimizeLegibility;
 }
 
 .error {
@@ -31,4 +35,9 @@ body {
 .mat-card-actions .mat-button-base,
 .mat-dialog-actions .mat-button-base {
   text-transform: uppercase;
+}
+
+.cdk-drag-animating,
+.cdk-drop-list-dragging .cdk-drag {
+  transition: var(--transition);
 }

commit 7f50204aeea10c31c6e7cfffdaac1f438a9acfb3

Fix long objective display issues (closes #155) Problem was introduced in 47d5d4b.

backend/in_memory_storage/in_memory_storage.go
@@ -249,6 +249,18 @@ func makeFakePeriod(id string) models.Period {
 					Tags: []models.ObjectiveTag{},
 				},
 				{
+					Name: "A really long objective name to show what happens when these things get really " +
+						"very long indeed. Look, this objective really is quite long, and it's good to know " +
+						"what happens in the UI in that case. If I just keep going and going and going, does " +
+						"the text start to overlap with other objectives? Let's add even more text to make this " +
+						"really really really really clear",
+					ResourceEstimate: 5,
+					CommitmentType:   "Committed",
+					Assignments:      []models.Assignment{},
+					Groups:           []models.ObjectiveGroup{},
+					Tags:             []models.ObjectiveTag{},
+				},
+				{
 					Name:             "Fourth objective",
 					ResourceEstimate: 8,
 					CommitmentType:   "Aspirational",

src/app/bucket/bucket.component.css
@@ -19,6 +19,16 @@ app-bucket .mat-list-item-content {
   display: flex;
 }
 
+.app-mat-list-item {
+  height: auto !important;
+  padding-bottom: 15px !important;
+}
+
+.reorder-icon {
+  vertical-align: middle;
+  padding-right: 10px;
+}
+
 app-bucket .cdk-drag-placeholder {
   background-color: rgba(0, 0, 0, 0.04);
   cursor: move;

src/app/bucket/bucket.component.html
@@ -21,7 +21,7 @@
 </mat-card-header>
 <mat-card-content>
 <mat-list dense *ngIf="!showOrderButtons">
-  <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives">
+  <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives" class="app-mat-list-item">
     <ng-container *ngTemplateOutlet="objective; context: {$implicit: displayObjective}">
     </ng-container>
   </mat-list-item>
@@ -29,9 +29,9 @@
 <mat-list dense *ngIf="showOrderButtons" cdkDropList (cdkDropListDropped)="reorderDrop($event)">
   <mat-list-item *ngFor="let displayObjective of bucket!.objectives | displayObjectives"
       cdkDrag
-      [cdkDragPreviewClass]="['cdk-drag-preview-app-bucket', 'mat-elevation-z1']">
+      [cdkDragPreviewClass]="['cdk-drag-preview-app-bucket', 'mat-elevation-z1']" class="app-mat-list-item">
     <div *cdkDragPlaceholder></div>
-    <i class="material-icons" cdkDragHandle>reorder</i>
+    <i class="material-icons reorder-icon" cdkDragHandle>reorder</i>
     <ng-container *ngTemplateOutlet="objective; context: {$implicit: displayObjective}">
     </ng-container>
   </mat-list-item>