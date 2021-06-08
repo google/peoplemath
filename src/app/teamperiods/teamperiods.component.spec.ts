583fff713f25ea9359bed003b046106f4f716f56
Andrew Medworth <amedworth@google.com>
2021-06-08 19:48:02 +0100

    Fix copying of secondary units (#161)

--- src/app/teamperiods/teamperiods.component.spec.ts
+++ src/app/teamperiods/teamperiods.component.spec.ts
@@ -25,6 +25,35 @@ import { Person } from '../person';
 import { AngularFireModule } from '@angular/fire';
 import { firebaseConfig } from '../../environments/firebaseConfig';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/portal';
+import {
+  AddPeriodDialogComponent,
+  AddPeriodDialogData,
+  CreateMethod,
+} from '../add-period-dialog/add-period-dialog.component';
+import { ImmutablePeriod, ImmutableSecondaryUnit, Period } from '../period';
+import { ObjectUpdateResponse } from '../objectupdateresponse';
+
+// Class to mimic the MatDialog and apply user actions to the dialog data
+class MockDialog {
+  editor?: (d: AddPeriodDialogData) => void;
+
+  open(
+    component: ComponentType<AddPeriodDialogComponent>,
+    config?: MatDialogConfig<AddPeriodDialogData>
+  ) {
+    return {
+      afterClosed: () => {
+        const data = config!.data;
+        if (this.editor && data) {
+          this.editor(data);
+        }
+        return of(data);
+      },
+    };
+  }
+}
 
 describe('TeamPeriodsComponent', () => {
   let component: TeamPeriodsComponent;
@@ -32,7 +61,9 @@ describe('TeamPeriodsComponent', () => {
   const storageServiceSpy = jasmine.createSpyObj('StorageService', [
     'getTeam',
     'getPeriods',
+    'addPeriod',
   ]);
+  const dialogMock = new MockDialog();
   const TEST_TEAM = new Team('testTeam', 'My test team');
 
   beforeEach(
@@ -56,6 +87,7 @@ describe('TeamPeriodsComponent', () => {
               paramMap: of(convertToParamMap({ team: TEST_TEAM.id })),
             },
           },
+          { provide: MatDialog, useValue: dialogMock },
         ],
       }).compileComponents();
       storageServiceSpy.getTeam.and.returnValue(of(TEST_TEAM));
@@ -81,5 +113,65 @@ describe('TeamPeriodsComponent', () => {
       new Person('peter', 'Peter Parker', 'USA', 10),
     ];
     expect(component.copyPeople(people)).toEqual(people);
+  });
+
+  it('should copy secondary units correctly', () => {
+    const sourcePeriodID = 'sourceID';
+    component.periods = [
+      ImmutablePeriod.fromPeriod({
+        id: sourcePeriodID,
+        displayName: 'Source period',
+        unit: 'things',
+        secondaryUnits: [
+          new ImmutableSecondaryUnit({
+            name: 'semithings',
+            conversionFactor: 2,
+          }),
+        ],
+        notesURL: '',
+        maxCommittedPercentage: 50,
+        buckets: [],
+        people: [],
+        lastUpdateUUID: '',
+      }),
+    ];
+    const newPeriod: Period = {
+      id: 'new',
+      displayName: 'My copied period',
+      unit: '',
+      secondaryUnits: [],
+      maxCommittedPercentage: -1,
+      notesURL: '',
+      buckets: [],
+      people: [],
+      lastUpdateUUID: '',
+    };
+    dialogMock.editor = (data) => {
+      data.period = newPeriod;
+      data.createMethod = CreateMethod.Copy;
+      data.copyFromPeriodID = 'sourceID';
+      data.copyUnit = true;
+      data.copyPeople = false;
+      data.copyBuckets = false;
+      data.copyObjectives = false;
+      data.copyAssignments = false;
+    };
+    storageServiceSpy.getPeriods.and.returnValue(of([])); // Not a realistic value, but it doesn't matter
+    const resp: ObjectUpdateResponse = {
+      lastUpdateUUID: 'foo',
+    };
+    storageServiceSpy.addPeriod.and.returnValue(of(resp));
+    component.addPeriod();
+    const expectedPeriod: Period = {
+      ...newPeriod,
+      unit: 'things',
+      maxCommittedPercentage: 50,
+      secondaryUnits: [{ name: 'semithings', conversionFactor: 2 }],
+      lastUpdateUUID: 'foo', // By the time we do the comparison, this will have been set
+    };
+    expect(storageServiceSpy.addPeriod).toHaveBeenCalledOnceWith(
+      TEST_TEAM.id,
+      expectedPeriod
+    );
   });
 });
