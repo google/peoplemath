d41d93a04758440ce81b6fc7bebc542050a08636
Andrew Medworth <amedworth@google.com>
2021-11-29 17:25:38 +0000

    Unset block ID when moving objective to new bucket (#186) (#20)

--- src/app/edit-objective-dialog/edit-objective-dialog.component.ts
+++ src/app/edit-objective-dialog/edit-objective-dialog.component.ts
@@ -147,7 +147,10 @@ export class EditObjectiveDialogComponent {
   }
 
   onMove(newBucket: ImmutableBucket): void {
-    const newObjective = makeObjective(this.data.objective);
+    let oldObjective = { ...this.data.objective };
+    // Block ID is no longer meaningful in the new bucket
+    oldObjective.blockID = undefined;
+    const newObjective = makeObjective(oldObjective);
     this.data.onMoveBucket!.emit([
       this.data.original!,
       newObjective,
