e1c404318d07a9ac6c496d0eabc58f27e52a286b
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-04 21:15:30 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,14 @@
 
 import { enableProdMode /*, ApplicationRef*/ } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableDebugTools } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-// import { enableDebugTools } from '@angular/platform-browser';
-
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  // Uncomment to enable console debug tools, such as ng.profiler.timeChangeDetection()
-  /*.then(moduleRef => {
-  const applicationRef = moduleRef.injector.get(ApplicationRef);
-  const componentRef = applicationRef.components[0];
-  enableDebugTools(componentRef);
-})*/
   .catch((err) => console.log(err));
