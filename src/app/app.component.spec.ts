3545553211794d2f30f9eac8195c130831e661ba
Andrew Medworth <amedworth@google.com>
2023-11-24 15:24:57 +0000

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -50,12 +50,12 @@ describe('AppComponent', () => {
   );
 
   it(
-    'should render title in a h1 tag',
+    'should render title',
     waitForAsync(() => {
       const fixture = TestBed.createComponent(AppComponent);
       fixture.detectChanges();
       const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain('PeopleMath');
+      expect(compiled.querySelector('.headername').textContent).toContain('PeopleMath');
     })
   );
 
